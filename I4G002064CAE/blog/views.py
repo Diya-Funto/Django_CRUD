61ed72ffac81697542e39bda4bf350a9aa60cecb
Funto Sofidiya <funto.s@yahoo.com>
2022-06-25 22:13:14 +0100

    more changes

--- I4G002064CAE/blog/views.py
+++ I4G002064CAE/blog/views.py
@@ -1,32 +1,33 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+#from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
-from blog.models import Post
+from .models import Post
+
 
 class PostListView(ListView):
     model = Post
-    template_name = 'list.html'
+    #template_name = "blog/post_list.html"
 
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
-    success_url = reverse_lazy("blog:all")
-    template_name = 'home.html'
+    success_url = reverse_lazy('blog:all')
+    #template_name = 'blog/post_home.html'
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'detail.html'
+    #template_name = 'blog/post_detail.html'
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
-    success_url = reverse_lazy("blog:all")
-    template_name = 'update.html'
+    success_url = reverse_lazy('blog:all')
+    #template_name = 'blog/post_update.html'
 
 class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
-    success_url = reverse_lazy("blog:all")
-    template_name = 'delete.html'
+    success_url = reverse_lazy('blog:all')
+    #template_name = 'blog/post_delete.html'
